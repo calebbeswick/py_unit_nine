4c72c94e829e2a8b1e32a06d13f65f396fedbbce
calebbeswick <beswica@ssfs.org>
2021-12-08 11:55:47 -0500

    worked on cards and classes

--- deck.py
+++ deck.py
@@ -1,2 +1,9 @@
+import card
 
 
+class Deck:
+
+    def __init__(self,):
+        self.cards = []
+        ace_spades = card.Card("Ace", "Clubs")
+        self.cards.append(ace_spades)
--- testing_cards.py
+++ testing_cards.py
@@ -1,1 +1,7 @@
+import card
 
+new_card = card.Card(9, "Hearts")
+print(new_card.suit)
+
+other_card = card.Card(5, "Clubs")
+print(other_card.rank)
