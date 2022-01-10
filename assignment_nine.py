cbe10823fc477ebc4b29bc7d1e091031b28b2a79
calebbeswick <beswica@ssfs.org>
2022-01-10 10:00:59 -0500

    working on unit nine project (blackjack)

--- assignment_nine.py
+++ assignment_nine.py
@@ -2,11 +2,35 @@ import card
 import deck
 
 def create_deck():
-    new_deck = deck.Deck()
-    new_deck.shuffle()
-    return new_deck
+    newdeck = deck.Deck()
+    newdeck.shuffle()
+    return newdeck
 
-def deal_hand(new_deck):
+def deal_cards(new_deck):
     hand = []
-    new_deck.pop[0]
-    new_deck.pop[1]
+    card1 = new_deck.pop()
+    card2 = new_deck.pop()
+    print(card1)
+    print(card2)
+    return hand
+
+# def player_turn(player_hand, newdeck ):
+#     new_deck = create_deck()
+#     deal_cards(new_deck)
+#
+# def dealers_turn():
+#     pass
+
+def is_blackjack(hand):
+    card1 = hand.pop(0)
+    card2 = hand.pop(1)
+    rank = card1.card.get_rank()
+    print(rank)
+
+# def who_won():
+
+
+
+new_deck = create_deck()
+card1 = deal_cards(new_deck)
+
